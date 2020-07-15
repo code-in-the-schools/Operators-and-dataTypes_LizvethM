c75298e3eeaeeb92bdd0cb008b50de041a6c2583
LizvethM <morales-garciaLizveth2021@email.com>
2020-07-15 17:19:05 +0000

    DataTypes_LizvethM

---
+++ main.py
@@ -0,0 +1,28 @@
+janRainfall2019 = 3.15
+febRainfall2019 = 3.64
+marRainfall2019 = 4.14
+aprRainfall2019 = 4.12
+mayRainfall2019 = 3.24
+junRainfall2019 = 3.65
+julRainfall2019 = 4.54
+augRainfall2019 = 3.34
+septRainfall2019 = 3.27
+octRainfall2019 = 4.45
+novRainfall2019 = 3.56
+decRainfall2019 = 4.37
+
+springRainfall2019 = marRainfall2019+aprRainfall2019+mayRainfall2019
+
+summerRainfall2019 = junRainfall2019+julRainfall2019+augRainfall2019
+
+fallRainfall2019 = septRainfall2019+octRainfall2019+novRainfall2019
+
+winterRainfall2019 = decRainfall2019+janRainfall2019+febRainfall2019
+
+print("The total spring rainfall is "+ str(springRainfall2019))
+print("The average spring rainfall is "+str(springRainfall2019/3))
+
+print()
+print("The total summer rainfall is "+ str(summerRainfall2019))
+print("The average summer rainfall is "+str(summerRainfall2019/3))
+
